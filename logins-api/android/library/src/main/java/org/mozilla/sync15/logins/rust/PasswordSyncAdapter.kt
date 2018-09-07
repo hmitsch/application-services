559c25b1358773f58aee209d2de01c42d5cc6277
Thom Chiovoloni <tchiovoloni@mozilla.com>
2018-09-06 17:48:16 -0700

    Point kotlin at FFI code, update kotlin side of FFI with changes

--- logins-api/android/library/src/main/java/org/mozilla/sync15/logins/rust/PasswordSyncAdapter.kt
+++ logins-api/android/library/src/main/java/org/mozilla/sync15/logins/rust/PasswordSyncAdapter.kt
@@ -55,7 +55,10 @@ internal interface PasswordSyncAdapter : Library {
     fun sync15_passwords_touch(state: RawLoginSyncState, id: String, error: RustError.ByReference)
     fun sync15_passwords_delete(state: RawLoginSyncState, id: String, error: RustError.ByReference): Boolean
 
-    fun destroy_c_char(p: Pointer)
+    fun sync15_passwords_add(state: RawLoginSyncState, new_login_json: String, error: RustError.ByReference)
+    fun sync15_passwords_update(state: RawLoginSyncState, existing_login_json: String, error: RustError.ByReference)
+
+    fun sync15_passwords_destroy_string(p: Pointer)
 }
 
 class RawLoginSyncState : PointerType()
